b8e431ced82dd1a0c53a49cb124f4170b25f263b
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-09-08 18:24:31 -0400

    ansible: fix some tasks/scripts and add libvirt

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Send documents"
-rsync -avh --progress --update /home/AmarOk/Documents AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/Games AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/Pictures AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/TODOLists AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/backup AmarOk@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/Documents amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/Games amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/Pictures amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/TODOLists amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/backup amarok@192.168.1.2:/media/encrypted_backup/
 echo "Backup apps"
-rsync -avh --progress --update /home/AmarOk/.thunderbird AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.ssh AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.ansible AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.gnupg AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.local/share/jami AmarOk@192.168.1.2:/media/encrypted_backup/.local/share/
-rsync -avh --progress --update /home/AmarOk/.mozilla AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.oh-my-zsh AmarOk@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.password-store AmarOk@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.thunderbird amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.ssh amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.ansible amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.gnupg amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.local/share/jami amarok@192.168.1.2:/media/encrypted_backup/.local/share/
+rsync -avh --progress --update /home/AmarOk/.mozilla amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.oh-my-zsh amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/AmarOk/.password-store amarok@192.168.1.2:/media/encrypted_backup/
 echo "Send medias"
-rsync -avh --progress --update /home/AmarOk/Music/* AmarOk@192.168.1.2:/media/media_drive/
-rsync -avh --progress --update /home/AmarOk/Videos/* AmarOk@192.168.1.2:/media/media_drive/
+rsync -avh --progress --update /home/AmarOk/Music/* amarok@192.168.1.2:/media/media_drive/
+rsync -avh --progress --update /home/AmarOk/Videos/* amarok@192.168.1.2:/media/media_drive/
 rm -rf /home/AmarOk/Videos/* /home/AmarOk/Music/*
 
 echo `date` >> ~/.backup
--- scripts/backup_enconn.sh
+++ scripts/backup_enconn.sh
@@ -2,7 +2,7 @@
 
 rsync -avh --progress --update /etc /media/encrypted_backup/backup_enconn/
 rsync -avh --progress --update /var/www /media/encrypted_backup/backup_enconn/
-rsync -avh --progress --update /home/gitea /media/encrypted_backup/backup_enconn/home
-rsync -avh --progress --update /home/AmarOk /media/encrypted_backup/backup_enconn/home
+rsync -avh --progress --update /home/git /media/encrypted_backup/backup_enconn/home
+rsync -avh --progress --update /home/amarok /media/encrypted_backup/backup_enconn/home
 
 echo `date` >> ~/.backup
