b6c83f8fab224382f8c3bb58960d537eb06b3b36
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-06-17 17:09:02 -0400

    ansible: add yubikey and update android-studio

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "Send documents"
-rsync -avh --progress --update /home/AmarOk/Documents amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/Games amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/Pictures amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/TODOLists amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/backup amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/Documents amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/Games amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/Pictures amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/TODOLists amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/backup amarok@192.168.1.2:/media/encrypted_backup/
 echo "Backup apps"
-rsync -avh --progress --update /home/AmarOk/.thunderbird amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.ssh amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.ansible amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.gnupg amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.local/share/jami amarok@192.168.1.2:/media/encrypted_backup/.local/share/
-rsync -avh --progress --update /home/AmarOk/.mozilla amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.oh-my-zsh amarok@192.168.1.2:/media/encrypted_backup/
-rsync -avh --progress --update /home/AmarOk/.password-store amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.thunderbird amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.ssh amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.ansible amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.gnupg amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.local/share/jami amarok@192.168.1.2:/media/encrypted_backup/.local/share/
+rsync -avh --progress --update /home/amarok/.mozilla amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.oh-my-zsh amarok@192.168.1.2:/media/encrypted_backup/
+rsync -avh --progress --update /home/amarok/.password-store amarok@192.168.1.2:/media/encrypted_backup/
 echo "Send medias"
-rsync -avh --progress --update /home/AmarOk/Music/* amarok@192.168.1.2:/media/media_drive/
-rsync -avh --progress --update /home/AmarOk/Videos/* amarok@192.168.1.2:/media/media_drive/
-rm -rf /home/AmarOk/Videos/* /home/AmarOk/Music/*
+rsync -avh --progress --update /home/amarok/Music/* amarok@192.168.1.2:/media/media_drive/
+rsync -avh --progress --update /home/amarok/Videos/* amarok@192.168.1.2:/media/media_drive/
+rm -rf /home/amarok/Videos/* /home/amarok/Music/*
 
 echo `date` >> ~/.backup
